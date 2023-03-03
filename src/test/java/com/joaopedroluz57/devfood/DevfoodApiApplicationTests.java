eebbd7f1ba7916a37ef5dfd746e8f299044d1cd0
João Pedro Luz <joao.pedro.luz@hotmail.com>
2023-03-03 19:42:35 -0300

    create project

---
+++ src/test/java/com/joaopedroluz57/devfood/DevfoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaopedroluz57.devfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevfoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
