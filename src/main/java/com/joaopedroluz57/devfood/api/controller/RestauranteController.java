6e82d762e396a476bb89948ff4eb581aeaf7d3d0
João Pedro Luz <joao.pedro.luz@hotmail.com>
2023-03-15 17:32:38 -0300

    feat: find all restaurants route

---
+++ src/main/java/com/joaopedroluz57/devfood/api/controller/RestauranteController.java
@@ -0,0 +1,24 @@
+package com.joaopedroluz57.devfood.api.controller;
+
+import com.joaopedroluz57.devfood.domain.model.Restaurante;
+import com.joaopedroluz57.devfood.domain.repository.RestauranteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/restaurantes")
+public class RestauranteController {
+
+    @Autowired
+    private RestauranteRepository restauranteRepository;
+
+    @GetMapping()
+    public List<Restaurante> listar() {
+        return restauranteRepository.todos();
+    }
+
+}
