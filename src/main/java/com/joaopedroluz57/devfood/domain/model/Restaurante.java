5945d988643afef99c27ef3e5a9f44124efe72d0
João Pedro Luz <joao.pedro.luz@hotmail.com>
2023-03-14 22:59:27 -0300

    add lombok

--- src/main/java/com/joaopedroluz57/devfood/domain/model/Restaurante.java
+++ src/main/java/com/joaopedroluz57/devfood/domain/model/Restaurante.java
@@ -1,16 +1,21 @@
 package com.joaopedroluz57.devfood.domain.model;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.math.BigDecimal;
-import java.util.Objects;
 
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 public class Restaurante {
 
     @Id
+    @EqualsAndHashCode.Include
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
@@ -18,40 +23,4 @@ public class Restaurante {
 
     private BigDecimal taxaEntrega;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public BigDecimal getTaxaEntrega() {
-        return taxaEntrega;
-    }
-
-    public void setTaxaEntrega(BigDecimal taxaEntrega) {
-        this.taxaEntrega = taxaEntrega;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Restaurante that = (Restaurante) o;
-        return Objects.equals(id, that.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
 }
