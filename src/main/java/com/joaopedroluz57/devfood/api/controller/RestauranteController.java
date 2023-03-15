3c183765845466dabc1db9f99c98e8311c6dd4a5
João Pedro Luz <joao.pedro.luz@hotmail.com>
2023-03-15 17:55:28 -0300

    feat: find restaurant by id route

--- src/main/java/com/joaopedroluz57/devfood/api/controller/RestauranteController.java
+++ src/main/java/com/joaopedroluz57/devfood/api/controller/RestauranteController.java
@@ -4,6 +4,7 @@ import com.joaopedroluz57.devfood.domain.model.Restaurante;
 import com.joaopedroluz57.devfood.domain.repository.RestauranteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +20,11 @@ public class RestauranteController {
     @GetMapping()
     public List<Restaurante> listar() {
         return restauranteRepository.todos();
+    }
+
+    @GetMapping("/{restauranteId}")
+    public Restaurante buscar(@PathVariable Long restauranteId) {
+        return restauranteRepository.porId(restauranteId);
     }
 
 }
