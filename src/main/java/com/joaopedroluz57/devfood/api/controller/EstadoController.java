77712db737134547a706cc0f1d4c313bd899d7f0
João Pedro Luz <joao.pedro.luz@hotmail.com>
2023-03-16 23:47:29 -0300

    feat: find state by id

--- src/main/java/com/joaopedroluz57/devfood/api/controller/EstadoController.java
+++ src/main/java/com/joaopedroluz57/devfood/api/controller/EstadoController.java
@@ -3,11 +3,14 @@ package com.joaopedroluz57.devfood.api.controller;
 import com.joaopedroluz57.devfood.domain.model.Estado;
 import com.joaopedroluz57.devfood.domain.repository.EstadoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/estados")
@@ -19,6 +22,17 @@ public class EstadoController {
     @GetMapping
     public List<Estado> listar() {
         return estadoRepository.buscarTodos();
+    }
+    
+    @GetMapping("/{id}") 
+    public ResponseEntity<Estado> buscar(@PathVariable Long id) {
+        final Estado estado = estadoRepository.buscarPorId(id);
+
+        if(Objects.isNull(estado)) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok().body(estado);
     }
 
 }
