aa80834c91d5a039588f02ed6f154b85155cd20e
João Pedro Luz <joao.pedro.luz@hotmail.com>
2023-03-18 22:10:41 -0300

    refactor: others repositories

--- src/main/java/com/joaopedroluz57/devfood/domain/repository/FormaPagamentoRepository.java
+++ src/main/java/com/joaopedroluz57/devfood/domain/repository/FormaPagamentoRepository.java
@@ -1,17 +1,9 @@
 package com.joaopedroluz57.devfood.domain.repository;
 
 import com.joaopedroluz57.devfood.domain.model.FormaPagamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
-public interface FormaPagamentoRepository {
-
-    List<FormaPagamento> buscarTodas();
-
-    FormaPagamento buscarPorId(Long id);
-
-    FormaPagamento salvar(FormaPagamento formaPagamento);
-
-    void remover(FormaPagamento formaPagamento);
-
+@Repository
+public interface FormaPagamentoRepository extends JpaRepository<FormaPagamento, Long> {
 }
--- src/main/java/com/joaopedroluz57/devfood/domain/repository/PermissaoRepository.java
+++ src/main/java/com/joaopedroluz57/devfood/domain/repository/PermissaoRepository.java
@@ -1,17 +1,9 @@
 package com.joaopedroluz57.devfood.domain.repository;
 
 import com.joaopedroluz57.devfood.domain.model.Permissao;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
-public interface PermissaoRepository {
-
-    List<Permissao> buscarTodas();
-
-    Permissao buscarPorId(Long id);
-
-    Permissao salvar(Permissao permissao);
-
-    void remover(Permissao permissao);
-
+@Repository
+public interface PermissaoRepository extends JpaRepository<Permissao, Long> {
 }
