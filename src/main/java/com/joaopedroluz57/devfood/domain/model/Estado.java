bd1f517067d20643f36065c90ce6c19242807fe6
João Pedro Luz <joao.pedro.luz@hotmail.com>
2023-04-15 22:54:39 -0300

    feat: user entity

--- src/main/java/com/joaopedroluz57/devfood/domain/model/Estado.java
+++ src/main/java/com/joaopedroluz57/devfood/domain/model/Estado.java
@@ -2,8 +2,6 @@ package com.joaopedroluz57.devfood.domain.model;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
 
 import javax.persistence.*;
 
