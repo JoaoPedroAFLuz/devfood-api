7cef6869ed053888e58746e1a49e4dc67256b398
João Pedro Luz <joao.pedro.luz@hotmail.com>
2023-04-15 22:42:42 -0300

    feat: group entity

---
+++ src/main/java/com/joaopedroluz57/devfood/domain/model/Grupo.java
@@ -0,0 +1,25 @@
+package com.joaopedroluz57.devfood.domain.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Grupo {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    @ManyToMany
+    @JoinTable(name = "grupo_permissao",
+            joinColumns = @JoinColumn(name = "grupo_id"),
+            inverseJoinColumns = @JoinColumn(name = "permissao_id"))
+    private List<Permissao> permissoes;
+
+}
