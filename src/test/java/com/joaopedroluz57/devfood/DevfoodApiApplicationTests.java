e3e0b7845e9cc2ba54f2b9fa3fdbe982ef0df80a
João Pedro Luz <70025328+JoaoPedroAFLuz@users.noreply.github.com>
2023-11-19 23:57:00 -0300

    feat: prepare project to integration tests

---
+++ src/test/java/com/joaopedroluz57/devfood/DevfoodApiApplicationTests.java
@@ -0,0 +1,19 @@
+package com.joaopedroluz57.devfood;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.fail;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DevfoodApiApplicationTests {
+
+    @Test
+    public void contextLoads() {
+        fail();
+    }
+
+}
