cf347df633658e6809a239602d1e028e446fe154
João Pedro Luz <70025328+joaopedroafluz@users.noreply.github.com>
2023-11-20 21:07:03 -0300

    feat: find all kitchens api tests

---
+++ src/test/java/com/joaopedroluz57/devfood/CozinhaAPITests.java
@@ -0,0 +1,47 @@
+package com.joaopedroluz57.devfood;
+
+import io.restassured.http.ContentType;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class CozinhaAPITests {
+
+    @LocalServerPort
+    private int port;
+
+    @Test
+    public void deveRetornarStatus200_QuandoConsultarCozinhas() {
+        given()
+                .basePath("/cozinhas")
+                .port(port)
+                .accept(ContentType.JSON)
+                .when()
+                .get()
+                .then()
+                .statusCode(HttpStatus.OK.value());
+    }
+
+    @Test
+    public void deveRetornar4Cozinhas_QuandoConsultarCozinhas() {
+        given()
+                .basePath("/cozinhas")
+                .port(port)
+                .accept(ContentType.JSON)
+                .when()
+                .get()
+                .then()
+                .body("nome", hasSize(5))
+                .body("nome", hasItems("Tailandesa", "Indiana"));
+    }
+
+}
