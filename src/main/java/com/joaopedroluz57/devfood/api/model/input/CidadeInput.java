cabfc251b3207978eef085a7024d5d458f93be3c
João Pedro Luz <70025328+JoaoPedroAFLuz@users.noreply.github.com>
2024-01-21 09:30:11 -0300

    fix: city input model validations

--- src/main/java/com/joaopedroluz57/devfood/api/model/input/CidadeInput.java
+++ src/main/java/com/joaopedroluz57/devfood/api/model/input/CidadeInput.java
@@ -3,11 +3,17 @@ package com.joaopedroluz57.devfood.api.model.input;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 @Getter
 @Setter
 public class CidadeInput {
 
+    @NotBlank
     private String nome;
+
+    @NotNull
     private EstadoIdInput estado;
 
 }
--- src/main/java/com/joaopedroluz57/devfood/api/model/input/EstadoIdInput.java
+++ src/main/java/com/joaopedroluz57/devfood/api/model/input/EstadoIdInput.java
@@ -3,10 +3,13 @@ package com.joaopedroluz57.devfood.api.model.input;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotNull;
+
 @Getter
 @Setter
 public class EstadoIdInput {
 
+    @NotNull
     private Long id;
 
 }
