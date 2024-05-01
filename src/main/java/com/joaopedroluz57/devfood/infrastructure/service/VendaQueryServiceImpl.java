b630c592daebadb4f3b24f38d985266b327a6809
João Pedro Fortunato <70025328+JoaoPedroAFLuz@users.noreply.github.com>
2024-05-01 02:41:15 -0300

    fix: offset time handling

--- src/main/java/com/joaopedroluz57/devfood/infrastructure/service/VendaQueryServiceImpl.java
+++ src/main/java/com/joaopedroluz57/devfood/infrastructure/service/VendaQueryServiceImpl.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.criteria.*;
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeParseException;
 import java.util.*;
 
 @Repository
@@ -27,9 +29,17 @@ public class VendaQueryServiceImpl implements VendaQueryService {
         CriteriaQuery<VendaDiaria> query = builder.createQuery(VendaDiaria.class);
         Root<Pedido> root = query.from(Pedido.class);
 
-        Expression<Date> functionDateDataCriacao = builder.function(
-                "date", Date.class, root.get("dataCriacao")
+        String offset = this.getOffset(filtro);
+
+        Expression<Date> functionConvertTzDataCriacao = builder.function(
+                "convert_tz",
+                Date.class,
+                root.get("dataCriacao"),
+                builder.literal("+00:00"),
+                builder.literal(offset)
         );
+
+        Expression<Date> functionDateDataCriacao = builder.function("date", Date.class, functionConvertTzDataCriacao);
 
         CompoundSelection<VendaDiaria> selection = builder.construct(VendaDiaria.class,
                 builder.count(root.get("id")),
@@ -61,6 +71,34 @@ public class VendaQueryServiceImpl implements VendaQueryService {
         query.groupBy(functionDateDataCriacao);
 
         return entityManager.createQuery(query).getResultList();
+    }
+
+    private String getOffset(VendaDiariaFilter filtro) {
+        final String defaultOffset = "+00:00";
+
+        try {
+            if (Objects.isNull(filtro.getDataCriacaoInicio()) && Objects.isNull(filtro.getDataCriacaoFim())) {
+                return defaultOffset;
+            }
+
+            OffsetDateTime offsetDateTime;
+
+            if (Objects.nonNull(filtro.getDataCriacaoInicio())) {
+                offsetDateTime = filtro.getDataCriacaoInicio();
+            } else {
+                offsetDateTime = filtro.getDataCriacaoFim();
+            }
+
+            String offset = OffsetDateTime.parse(offsetDateTime.toString()).getOffset().toString();
+
+            if ("Z".equals(offset)) {
+                return defaultOffset;
+            }
+
+            return offset;
+        } catch (DateTimeParseException e) {
+            return defaultOffset;
+        }
     }
 
 }
